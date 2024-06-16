e9025d8b09aeb63f9bb595480fb1dec94ad7a8df
Mario Rios <marweb@gmail.com>
2024-06-15 21:53:37 -0500

    initial commit for api and frontend

---
+++ api/ContactsApp/ContactsApp/Controllers/ContactsController.cs
@@ -0,0 +1,56 @@
+﻿using ContactsApp.Data;
+using ContactsApp.Models;
+using ContactsApp.Models.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactsApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly ContactsDbContext dbContext;
+
+        public ContactsController(ContactsDbContext dbContext) {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllContacts()
+        {
+            var contacts = dbContext.Contacts.ToList();
+            return Ok(contacts);
+        }
+        [HttpPost]
+        public IActionResult AddContact(AddContactRequestDTO request)
+        {
+            var domainModelContact = new Contact
+            {
+                Id = Guid.NewGuid(),
+                Name = request.Name,
+                Email = request.Email,
+                Phone = request.Phone,
+                Favorite = request.Favorite
+            };
+            dbContext.Contacts.Add(domainModelContact);
+            dbContext.SaveChanges();
+            return Ok(domainModelContact);
+
+        }
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public IActionResult DeleteContact(Guid id) {
+            var contact = dbContext.Contacts.Find(id);
+            if (contact is not null)
+            {
+                dbContext.Contacts.Remove(contact);
+                dbContext.SaveChanges();
+            }
+
+            return Ok();
+        }
+
+
+    }
+}
---
+++ api/ContactsApp/ContactsApp/Data/ContactsDbContext.cs
@@ -0,0 +1,12 @@
+﻿using ContactsApp.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactsApp.Data
+{
+    public class ContactsDbContext: DbContext
+    {
+        public ContactsDbContext(DbContextOptions<ContactsDbContext> options) : base(options) { 
+        }
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
